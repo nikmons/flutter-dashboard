77d52834a081a3042a372f74b8673922871d7199
nikos monahogios <nikos13@hotmail.com>
2020-04-28 17:45:39 +0300

    API endpoint

--- lib/services.dart
+++ lib/services.dart
@@ -3,12 +3,12 @@ import 'package:flutter/foundation.dart';
 class ApiSettings {
 
   static String GetApiEndpoint() {
-    print("kRelaseMode is [" + kReleaseMode.toString() + "]");
-    if (kReleaseMode) {
+    //print("kRelaseMode is [" + kReleaseMode.toString() + "]");
+    //if (kReleaseMode) {
       return 'https://eu-vs-covid.herokuapp.com/api/';
-    } else {
+    //} else {
       return 'http://127.0.0.1:5000/api/';
-    }
+    //}
   }
 }
 
